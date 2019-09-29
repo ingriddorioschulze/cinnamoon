770a716e9d2bcbf47034cf845dd3461b9d506cc6
Ingrid do Rio Schulze <ingriddorio@gmail.com>
2019-09-29 15:25:48 +0200

    add routing to the pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { Switch, Route } from "react-router-dom";
+
+import "./App.css";
+
 import HomePage from "./pages/homepage/homepage.component"
 
 function App() {
