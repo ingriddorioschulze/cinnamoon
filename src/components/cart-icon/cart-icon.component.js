f51f2aa6543f609c0a61364a81c11695d1ee9b80
ingriddorioschulze <ingriddorio@gmail.com>
2020-03-04 11:39:12 +0100

    implement redux for cart item

--- src/components/cart-icon/cart-icon.component.js
+++ src/components/cart-icon/cart-icon.component.js
@@ -8,7 +8,7 @@ import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg"
 import "./cart-icon.styles.scss"
 
 const CartIcon = ({toggleCartHidden}) => (
-  <div classname="cart-icon" onClick={toggleCartHidden}>
+  <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
     <span className="item-count">0</span>
   </div>
