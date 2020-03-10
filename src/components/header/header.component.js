8547dae624ee997e6ee3d7d27e84fb1597d08002
ingriddorioschulze <ingriddorio@gmail.com>
2020-03-10 09:42:29 +0100

    implement and refactor code using selectors

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { createStructuredSelector } from "reselect"
 
 import { auth } from '../../firebase/firebase.utils'
+import { selectCartHidden } from "../../redux/cart/cart.selectors"
+import { selectCurrentUser } from "../../redux/user/user.selectors"
 import CartIcon from "../../components/cart-icon/cart-icon.component"
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component'
 
@@ -29,9 +32,9 @@ const Header = ({ currentUser, hidden }) => (
   </div>
 )
 
-const mapStateToProps = ({user: { currentUser }, cart:{ hidden }}) => ({
-  currentUser,
-  hidden
+const mapStateToProps = createStructuredSelector ({
+  currentUser: selectCurrentUser, 
+  hidden: selectCartHidden,
 })
 
 export default connect(mapStateToProps)(Header)
