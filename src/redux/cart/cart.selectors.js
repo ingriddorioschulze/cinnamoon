88dd28064596c4cc21fc1dfe330088ea40855882
ingriddorioschulze <ingriddorio@gmail.com>
2020-03-11 10:07:29 +0100

    add chekout component

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -12,12 +12,22 @@ export const selectCartHidden = createSelector(
   (cart) => cart.hidden
 )
 
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce(
+      (accumulatedQuantity, cartItem) =>
+        accumulatedQuantity + cartItem.quantity,
+      0,
+    ),
+)
+
 export const selectCartItemsCount = createSelector(
   [selectCartItems],
   cartItems =>
     cartItems.reduce(
       (accumulatedQuantity, cartItem) =>
-      accumulatedQuantity + cartItem.quantity,
+      accumulatedQuantity + cartItem.quantity * cartItem.price,
       0,
     ),
 )
